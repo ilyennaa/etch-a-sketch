25b334f5228c0b039af68a9b6cda66655ff428a2
Ilyenna <jwelsh518@gmail.com>
2023-01-10 14:48:14 -0500

    Work on tile event listeners
    Trying to figure out how to attach an event listener to each tile in the board.

--- script.js
+++ script.js
@@ -2,25 +2,29 @@ const board = document.querySelector('.board');
 
 populateBoard();
 
+const tile = board.childNodes;
+console.log(tile[0].childNodes);
 
+
+const tiles = document.querySelectorAll('.rowDiv>.tile');
+// console.log(tile);
 
 function populateBoard() {
     // create horizontal divs
     for (let i = 0; i < 16; i++) {
         let rowDiv = document.createElement('div');
-        rowDiv.classList.add(`.rowDiv${i+1}`);
+        rowDiv.classList.add('.rowDiv');
 
         rowDiv.style.height = "6.25%";
         rowDiv.style.width = "100%";
         rowDiv.style.display = "flex";
 
-        board.appendChild(rowDiv);
-
-        
+        board.appendChild(rowDiv);        
     }
 
      // populate horizontal divs with squares
     const rowDivs = board.childNodes;
+
     rowDivs.forEach(function() {
         for (let i = 0; i < rowDivs.length; i++) {
             let horizontalDivs = rowDivs[i];
