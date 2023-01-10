05c5667402dc6dde3931ab1c0a0993142286cb4e
Ilyenna <jwelsh518@gmail.com>
2023-01-10 14:21:03 -0500

    Completed "populateBoard" function

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
 const board = document.querySelector('.board');
 
-
 populateBoard();
+
+
 
 function populateBoard() {
     // create horizontal divs
     for (let i = 0; i < 16; i++) {
         let rowDiv = document.createElement('div');
-        rowDiv.classList.add('.rowDiv');
+        rowDiv.classList.add(`.rowDiv${i+1}`);
 
         rowDiv.style.height = "6.25%";
         rowDiv.style.width = "100%";
+        rowDiv.style.display = "flex";
 
         board.appendChild(rowDiv);
+
+        
     }
 
-    // console.log(board);
-    rowDivs = board.querySelectorAll('.rowDiv');
-    
-    
+     // populate horizontal divs with squares
+    const rowDivs = board.childNodes;
+    rowDivs.forEach(function() {
+        for (let i = 0; i < rowDivs.length; i++) {
+            let horizontalDivs = rowDivs[i];
+            let tile = document.createElement('div');
+            tile.classList.add('.tile');
 
-    // populate horizontal divs with squares
+            tile.style.height = "100%";
+            tile.style.width = "6.25%";
+            tile.style.backgroundColor = "white";
+
+            horizontalDivs.appendChild(tile);
+        }
+    });
 
 }
+
